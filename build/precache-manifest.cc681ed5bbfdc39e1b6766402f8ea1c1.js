ea987406419a7bbd4ac4fa624dfbc3ed6354e814
Sutthipong Thaisuriya <cg610076@ptt.corp>
2019-05-17 15:20:00 +0700

    All file

---
+++ build/precache-manifest.cc681ed5bbfdc39e1b6766402f8ea1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af46849ba6ca3d87a6d22a6f5e6c11f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f80fd95e90cd847fb2",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3faea562172d802682ef",
+    "url": "/static/js/2.d38c7d91.chunk.js"
+  },
+  {
+    "revision": "20f80fd95e90cd847fb2",
+    "url": "/static/js/main.81a0a587.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const data = async (item, size) => {
+  item = JSON.parse(item)
+  let nodeDataArray =  item.nodeDataArray.map(node => {
+    let font = (node.font) ? size + "px" + " sans-serif" : ''
+    return {
+      ...node,
+      font
+    }
+  })
+  return {
+    ...item,
+    nodeDataArray
+  }
+}
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      old: '',
+      new:'',
+      size: 0
+
+    }
+  }
+
+userClick = async () =>{
+  let newData = await data(this.state.old,this.state.size)
+  this.setState({
+    new: newData
+  })
+
+}
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="font size" onChange={(e)=> this.setState({size: e.target.value})}/>
+      <textarea placeholder="json" onChange={(even)=>this.setState({old: even.target.value})}/>
+      <button onClick={this.userClick}>Renew</button>
+      <textarea placeholder="json" value={JSON.stringify(this.state.new)}/>
     </div>
   );
 }
 
-export default App;
+}
